3f2730e5be13081c4b25bf551868f44d857509c5
Joshua Fraser <jfraser@apcera.com>
2015-02-07 01:03:27 -0800

    using ringbuffer to store last value. will push this out to a set of storage functions. also added ttl faker

--- dingo.go
+++ dingo.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"container/ring"
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/jfray/dingo/lookup"
 )
 
 func main() {
-
-	var oldSerial uint32
 	debug := flag.Bool("debug", false, "Debug Mode?")
+	ringSize := flag.Int("ringsize", 32, "How big should I make the ringbuffer?")
+	fakeTtl := flag.Bool("fakettl", false, "Should I fake the ttl?")
+	fakeTtlInterval := flag.Int("fakettl_interval", 10, "How long should the fake TTL be?")
 	flag.Parse()
 	
 	if len(flag.Args()) == 0 {
@@ -20,17 +23,43 @@ func main() {
 		os.Exit(1)
 	}
 
+
+	r := ring.New(*ringSize)
+	r.Value = 0
+	r = r.Next()
+
 	for {
+		var actualTtl uint32
+
 		serial, ttl := lookup.GetSOA(flag.Arg(0))
+
+		if (*fakeTtl) {
+			actualTtl = uint32(*fakeTtlInterval)
+		} else {
+			actualTtl = ttl
+		}
+
+		o := r.Prev()
+
+		// This can't be right
+		oi, _ := strconv.ParseUint(fmt.Sprintf("%d", o.Value), 0, 64)
+		oldSerial := uint32(oi)
+		
+		r.Value = serial
+		r = r.Next()
+
+		r.Do(func(x interface{}) {
+			fmt.Println(x)
+		})
+
 		if (oldSerial > 0) && (serial > oldSerial) {
 			fmt.Printf("Hey dude, we updated %d to %d", oldSerial, serial)
 			break
 		}
 		if (*debug) {
-        		fmt.Printf("My Serial Number is: %d & and its TTL is %d, previous serial was %d\n", serial, ttl, oldSerial)
-			fmt.Printf("Sleeping %d seconds\n", ttl)
+        		fmt.Printf("My Serial Number is: %d & and its TTL is %d, previous serial was %d\n", serial, actualTtl, oldSerial)
+			fmt.Printf("Sleeping %d seconds\n", actualTtl)
 		}
-		oldSerial = serial
 		if (*debug) {
 			ticker := time.NewTicker(time.Second * 1)
 		
@@ -39,10 +68,11 @@ func main() {
             				fmt.Println("Tick at", t)
         			}
     			}()
-			time.Sleep(time.Duration(ttl) * time.Second)
+			
+			time.Sleep(time.Duration(actualTtl) * time.Second)
 			ticker.Stop()
 		} else {
-			time.Sleep(time.Duration(ttl) * time.Second)
+			time.Sleep(time.Duration(actualTtl) * time.Second)
 		}
 	}
 }

d21eac5b9848d2dd6e72a922d134cfec0986bd94
Joshua Fraser <jfraser@apcera.com>
2015-02-06 23:09:04 -0800

    moving some stuff around

--- dingo.go
+++ dingo.go
@@ -2,31 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/miekg/dns"
 	"os"
 	"time"
+
+	"github.com/jfray/dingo/lookup"
 )
-
-func lookup(domain string) (serial uint32, ttl uint32) {
-	m := new(dns.Msg)
-	m.RecursionDesired = true
-	m.Question = make([]dns.Question, 1)
-	
-	m.Question[0] = dns.Question{dns.Fqdn(domain), 
-			dns.TypeSOA, dns.ClassINET}
-        
-	c := new(dns.Client)
-        in, _, _ := c.Exchange(m, "8.8.8.8:53")
-
-	serial = in.Answer[0].(*dns.SOA).Serial
-	ttl = in.Answer[0].(*dns.SOA).Minttl
-	
-	return serial, ttl
-}
-
-func checkRecord(hostRecord string) bool {
-	return true
-}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -37,7 +17,7 @@ func main() {
 	var oldSerial uint32
 
 	for {
-		serial, ttl := lookup(os.Args[1])
+		serial, ttl := Lookup(os.Args[1])
 		if (oldSerial > 0) && (serial > oldSerial) {
 			fmt.Printf("Hey dude, we updated %d to %d", oldSerial, serial)
 			host := fmt.Sprintf("www.%s", os.Args[1])
---
+++ lookup/lookup.go
@@ -0,0 +1,33 @@
+package lookup
+
+import (
+	"fmt"
+	"github.com/miekg/dns"
+	"os"
+	"time"
+)
+
+func GetSOA(domain string) (serial uint32, ttl uint32) {
+	conf, err := dns.ClientConfigFromFile("/etc/resolv.conf")
+	if conf == nil {
+		fmt.Printf("Cannot initialize the local resolver: %s\n", err)
+		os.Exit(1)
+	}
+
+	nsAddressPort := fmt.Sprintf("%s:53", conf.Servers[0])
+
+	m := new(dns.Msg)
+	m.RecursionDesired = true
+	m.Question = make([]dns.Question, 1)
+	
+	m.Question[0] = dns.Question{dns.Fqdn(domain), 
+			dns.TypeSOA, dns.ClassINET}
+        
+	c := new(dns.Client)
+        in, _, _ := c.Exchange(m, nsAddressPort)
+
+	serial = in.Answer[0].(*dns.SOA).Serial
+	ttl = in.Answer[0].(*dns.SOA).Minttl
+	
+	return serial, ttl
+}
